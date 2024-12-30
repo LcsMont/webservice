17541bae8664645ec77c08195ca31d2369a7a1b4
Lucas Monteiro <lucasmmnm@gmail.com>
2024-12-30 14:48:53 -0300

    Service layer, component registration

---
+++ src/main/java/com/lcsmont/webservices/services/UserService.java
@@ -0,0 +1,25 @@
+package com.lcsmont.webservices.services;
+
+import com.lcsmont.webservices.entities.User;
+import com.lcsmont.webservices.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
+}
